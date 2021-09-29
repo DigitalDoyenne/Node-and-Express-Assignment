d78f496f1ae1f0ed0ba81503822b4639cab8612e
digitaldoyenne <digitaldoyenne@protonmail.com>
2021-09-29 14:47:11 -0500

    Editing middleware

--- src/middleware/validateZip.js
+++ src/middleware/validateZip.js
@@ -1,6 +1,6 @@
 const validateZip = (req, _res, next) => {
   const zip = req.params.zip;
-  if (zip !== "all" && (zip.length !== 5 || typeof zip !== "number")) {
+  if (zip !== "all" && (zip.length !== 5 || isNaN(zip))) {
     next(`Zip (${zip}) is invalid!`);
   } else {
   next()
